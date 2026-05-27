3c6595e77f202c3874932743f291e5a403fd5c6b
mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-27 17:39:56 +0200

    Add 2nd task

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module to serialize and deserialize an object and its content
+    with Pickle"""
+import pickle
+
+
+class CustomObject:
+    """Class represents a custom object"""
+    def __init__(self, name, age, is_student):
+        self.custname = name
+        self.custage = age
+        self.custis_student = is_student
+
+    def display(self):
+        print(f"Name: {self.custname}")
+        print(f"Age: {self.custage}")
+        print(f"Is Student: {self.custis_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(f"{filename}", "wb") as f:
+                pickle.dump(self, f)
+                return None
+        except (IOError, pickle.PicklingError, EOFError, Exception) as e:
+            print(f"Cant write in {filename}")
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj
+        except (IOError, pickle.UnpicklingError, EOFError, Exception) as e:
+            print(f"Cant read {filename}")
+            return None
