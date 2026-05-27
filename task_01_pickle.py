63e438df8aa6d137c67b88bfeb13a03b5e97c2ac
mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-27 17:37:06 +0200

    Add 2nd task

--- task_01_pickle.py
+++ task_01_pickle.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python3
-"""This module defines a CustomObject class 
-that can be serialized and deserialized using the pickle module."""
+"""Module to serialize and deserialize an object and its content
+    with Pickle"""
 import pickle
 
 
 class CustomObject:
-    """Class representing a custom object"""
+    """Class represents a custom object"""
     def __init__(self, name, age, is_student):
-        self.name = name
-        self.age = age
-        self.is_student = is_student
+        self.custname = name
+        self.custage = age
+        self.custis_student = is_student
 
     def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
+        print(f"Name: {self.custname}")
+        print(f"Age: {self.custage}")
+        print(f"Is Student: {self.custis_student}")
 
     def serialize(self, filename):
         try:
-            with open(filename, 'wb') as file:
-                pickle.dump(self, file)
+            with open(f"{filename}", "wb") as f:
+                pickle.dump(self, f)
+                return None
         except (IOError, pickle.PicklingError, EOFError, Exception) as e:
+            print(f"Cant write in {filename}")
             return None
 
     @classmethod
     def deserialize(cls, filename):
         try:
-            with open(filename, "rb") as file:
-                obj = pickle.load(file)
-            
-            if isinstance(obj, cls):
-                return obj
-            else:
-                return None
-        
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj
         except (IOError, pickle.UnpicklingError, EOFError, Exception) as e:
+            print(f"Cant read {filename}")
             return None
