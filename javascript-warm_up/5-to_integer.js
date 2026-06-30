1a2b607fd59a65b14e51a3207f858825ebcc054f
Nicolas J <nicolas.jacquemin26@gmail.com>
2026-06-30 15:47:31 +0200

    Task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,9 @@
 
 const arg = parseInt(process.argv[2]);
 
+
 if (isNaN(arg)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${arg}`);
+  console.log(`My number: ${arg}`);
 }
