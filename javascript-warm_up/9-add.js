f0d9387a187ef56f6b46a433b916a64018ccd808
Nicolas J <nicolas.jacquemin26@gmail.com>
2026-06-30 17:28:44 +0200

    Task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-
-function add(a, b) {
+function add (a, b) {
   const firstNumber = parseInt(a);
   const secNumber = parseInt(b);
   if (isNaN(firstNumber) || isNaN(secNumber)) {
